af260cf26bb0c5f1cd45d7da5456abcc59d28c7e
elena20ruiz <elena.ruiz.bdn@gmail.com>
2018-11-24 23:03:39 +0100

    Change enviorment variables

--- server/emotion/emotion_api.py
+++ server/emotion/emotion_api.py
@@ -1,9 +1,9 @@
 
 import requests
 import json
-from server import *
 
-
+MICROSOFT_KEY = '6921d35270394cbc83c543cb8e4cdd08'
+MICROSOFT_URL = 'https://westeurope.api.cognitive.microsoft.com/face/v1.0/detect'
 
 
 headers  = {'Ocp-Apim-Subscription-Key': MICROSOFT_KEY, "Content-Type": "application/octet-stream" }
--- server/spotify/calls.py
+++ server/spotify/calls.py
@@ -3,6 +3,96 @@ import json
 import requests
 import sys
 
+from server import *   #  GET KEYS
+
+# Workaround to support both python 2 & 3
+try:
+    import urllib.request, urllib.error
+    import urllib.parse as urllibparse
+except ImportError:
+    import urllib as urllibparse
+
+# ----------------- 0. SPOTIFY BASE URL ----------------
+
+SPOTIFY_API_BASE_URL = 'https://api.spotify.com'
+API_VERSION = "v1"
+SPOTIFY_API_URL = "{}/{}".format(SPOTIFY_API_BASE_URL, API_VERSION)
+
+# ----------------- 1. USER AUTHORIZATION ----------------
+
+# spotify endpoints
+SPOTIFY_AUTH_BASE_URL = "https://accounts.spotify.com/{}"
+SPOTIFY_AUTH_URL = SPOTIFY_AUTH_BASE_URL.format('authorize')
+SPOTIFY_TOKEN_URL = SPOTIFY_AUTH_BASE_URL.format('api/token')
+CLIENT_ID = 'bbba51ab47f34c39a9e9bc7503d73992'
+CLIENT_KEY = '39e30e7a896342a097891f855d004765'
+# server side parameter
+# * fell free to change it if you want to, but make sure to change in
+# your spotify dev account as well *
+CLIENT_SIDE_URL = "http://0.0.0.0"
+PORT = 5000
+REDIRECT_URI = "{}:{}/callback/".format(CLIENT_SIDE_URL, PORT)
+SCOPE = "playlist-modify-public playlist-modify-private user-read-recently-played user-top-read"
+STATE = ""
+SHOW_DIALOG_bool = True
+SHOW_DIALOG_str = str(SHOW_DIALOG_bool).lower()
+
+
+# https://developer.spotify.com/web-api/authorization-guide/
+auth_query_parameters = {
+    "response_type": "code",
+    "redirect_uri": REDIRECT_URI,
+    "scope": SCOPE,
+    # "state": STATE,
+    # "show_dialog": SHOW_DIALOG_str,
+    "client_id": CLIENT_ID
+}
+
+#python 3
+if sys.version_info[0] >= 3:
+    URL_ARGS = "&".join(["{}={}".format(key, urllibparse.quote(val))
+                    for key, val in list(auth_query_parameters.items())])
+
+
+AUTH_URL = "{}/?{}".format(SPOTIFY_AUTH_URL, URL_ARGS)
+
+'''
+    This function must be used with the callback method present in the
+    ../app.py file.
+    And of course this will only works if ouath == True
+'''
+
+
+def authorize(auth_token):
+
+    code_payload = {
+        "grant_type": "authorization_code",
+        "code": str(auth_token),
+        "redirect_uri": REDIRECT_URI
+    }
+    print('hi')
+    #python 3 or above
+    if sys.version_info[0] >= 3:
+        base64encoded = base64.b64encode(("{}:{}".format(CLIENT_ID, CLIENT_KEY)).encode())
+        headers = {"Authorization": "Basic {}".format(base64encoded.decode())}
+    else: 
+        base64encoded = base64.b64encode("{}:{}".format(CLIENT_ID, CLIENT_KEY))
+        headers = {"Authorization": "Basic {}".format(base64encoded)}
+
+    post_request = requests.post(SPOTIFY_TOKEN_URL, data=code_payload,
+                                 headers=headers)
+    print('h3')
+    # tokens are returned to the app
+    response_data = json.loads(post_request.text)
+    access_token = response_data["access_token"]
+
+    # use the access token to access Spotify API
+    auth_header = {"Authorization": "Bearer {}".format(access_token)}
+    return auth_header
+
+
+
+## FOR GET THE USER
 
 ## FOR FACE RECOGNITION
 def get_tracks_from_playlist_call(id, token):
